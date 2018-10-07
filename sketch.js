2b3d05715b09e3f7d64b438d4789c1baa1dd2c09
Giulia <giulia.ianes@gmail.com>
2018-10-07 18:45:15 +0200

    animated texture

--- sketch.js
+++ sketch.js
@@ -1,11 +1,71 @@
+var myColors = ['#CD5C5C', '#F08080', '#FA8072', '#E9967A', '#FFA07A'];
+//var
+
+var nColors = myColors.length;
+
+
+
+var cardWidth = 20;
+var cardHeight = 20;
+var cardScale = 1.5;
+var offset=cardWidth*2;
+
+var card1StartX = 0;
+var card1StartY = 0;
+
+var negativeY=50;
+var y=-negativeY;
+
+
+
+var numCarte = 25;
+
+
+//frameRate(40);
+
 function preload(){
-  // put preload code here
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(800,600);
+  background(234, 224, 204);
+  //background(0, 0, 0);
 }
 
 function draw() {
-  // put drawing code here
+
+var x=0;
+
+//y=(frameCount-negativeY)%height;
+
+y= random()*height;
+
+console.log('y='+y);
+//generate 50 shapes (columns) each loop
+  for (var i=-25; i<= numCarte; i++) {
+    //var y=frameCount-negativeY;
+    x+=noise(frameCount/50)*height;
+
+    //if(i%2==0) {stroke(255);}
+    //else {stroke(0,0,0);}
+    noStroke();
+    //fill(random(255), random(255), random(255));
+    //fill(lerpColor(color('#F6CEF5'),color('#29088A'), frameCount/120));
+    var colIndex;
+    if(i<0){
+      colIndex=(i*(-1))%nColors;
+    } else {
+      colIndex=i%nColors;
+    }
+    fill(myColors[colIndex]);
+
+    if(i%2==0){
+      rect(x+(offset*i), y, cardWidth*cardScale, cardHeight*cardScale);
+  } else {
+    ellipse(x+(offset*i), y, cardWidth*cardScale, cardHeight*cardScale);
+  }
+
+
+  }
+
 }
